9d508da9d872b2f9867a4067719a0c0138d31901
Vladislav Goloshchapov <kraidiky@gmail.com>
2015-08-25 21:48:13 +0200

    Промежуточный коммит, тащу домой код с рабочего компа.

--- Assets/Scripts/CollectableMapElement.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class CollectableMapElement : MonoBehaviour {
-    public string ReplaceElement;
-    // Когда-нибудь тут тоже будет какой-нибудь функционал
-    public virtual void DoSomething() { }
-}
--- Assets/Scripts/RotateButton.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class RotateButton : MonoBehaviour {
-	// Use this for initialization
-	void OnMouseUpAsButton() {
-        GameObject.FindObjectOfType<MapControler>().CurrentTetrisBlock.RotateElements();
-	}
-}
